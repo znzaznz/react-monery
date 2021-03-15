1791d3e256a13757a9b71e8509f46aea10ca68b9
xiao <779740337@qq.com>
2021-03-15 13:48:40 +0800

    完成svg的导入

--- src/component/Nav/index.jsx
+++ src/component/Nav/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import "./index.scss"
+import "./index.scss";
+require("../../icons/tags.svg");
 
 export default function index() {
     return (
@@ -8,7 +9,7 @@ export default function index() {
             <ul>
                 <li>
                     <svg>
-                        <use xlinkHref={""}/>
+                        <use xlinkHref="#tags"/>
                     </svg>
                     <Link to={"/tags"}>标签</Link>
                 </li>
