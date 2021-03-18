44bf72897b0f39e299a6b22ca0335350f72ef314
xiao <779740337@qq.com>
2021-03-18 18:47:09 +0800

    重构前的代码

--- src/views/Tags/Tags.jsx
+++ src/views/Tags/Tags.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {initializeNumber} from "../../model/initializeNumber";
 
 export default function Tags() {
+    const [tagsArr,setTagsArr] = useState(initializeNumber())
+
     return (
         <>
-            <h1>tags</h1>
+            <ol>
+                {tagsArr.map((item)=>{
+                    return <li key={item.id}>
+                        {item.name}
+
+                    </li>
+                })}
+            </ol>
         </>
     )
 }
