f33f5bd9a0d081e3601cb1079b5be4ee8d939014
xiao <779740337@qq.com>
2021-03-18 14:59:37 +0800

    完成设置收支函数

--- src/views/Money/CategorySection/index.jsx
+++ src/views/Money/CategorySection/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./index.scss"
 
-export default function index() {
+export default function Index() {
+    //定义收支情况
+    const [payment,setPayment] = useState("-")
+    const hashPayment = {"-":"支出","+":"收入"}
+
+    //选择收支的函数
+    const selectCategory = (category)=>{
+        setPayment(category)
+    }
+
     return (
         <div className={"CategorySection"}>
             <ol>
-                <li className={"selected"}>支出</li>
-                <li>收入</li>
+                <li className={payment === "-" ? "selected" : ""} onClick={()=>{selectCategory("-")}}>{hashPayment["-"]}</li>
+                <li className={payment === "+" ? "selected" : ""} onClick={()=>selectCategory("+")}>{hashPayment['+']}</li>
             </ol>
         </div>
     )
