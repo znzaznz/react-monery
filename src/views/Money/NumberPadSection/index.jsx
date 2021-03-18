d6c2a616370a32d5c0079dd533916b5fec72f677
xiao <779740337@qq.com>
2021-03-18 16:39:32 +0800

    完成数字键盘的功能

--- src/views/Money/NumberPadSection/index.jsx
+++ src/views/Money/NumberPadSection/index.jsx
@@ -4,18 +4,32 @@ import "./index.scss"
 export default function Index() {
     //初始化代码数据，并且得到showNum中的数字
     const buttonNum = ['1',"2",'3','删除','4','5','6','清空','7','8','9','OK','0','.']
-    const [showNum,setShowNum] = useState(0)
+    const [showNum,setShowNum] = useState("0")
 
     //设定button点击事件
     const clickButtonNum = (e)=>{
         const inputValue = e.target.textContent
         const charCode = inputValue.charCodeAt();
         if (charCode >= 46 && charCode <= 57){
-            console.log("数字");
+            if (showNum.length <= 17){
+                if (showNum === "0" && inputValue !== "."){
+                    return setShowNum(inputValue)
+                }
+                if (inputValue === "." && showNum.indexOf(".") !== -1){
+                    return showNum;
+                }
+                setShowNum(showNum+inputValue)
+            }else {
+                alert("数字过长")
+            }
         }else if (inputValue === "清空"){
-            console.log("清空");
+            setShowNum("0")
         }else if (inputValue === "删除"){
-            console.log("删除");
+            if (showNum.length === 1){
+                setShowNum("0")
+            }else {
+                setShowNum(showNum.slice(0,-1))
+            }
         }else {
             console.log("ok")
         }
