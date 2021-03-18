4bd92dc712e600f748927e35c5f29d99042c34f1
xiao <779740337@qq.com>
2021-03-18 14:49:30 +0800

    完成获取note的value

--- src/views/Money/NoteSection/index.jsx
+++ src/views/Money/NoteSection/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import "./index.scss"
 
-export default function index() {
+export default function Index() {
+    const inputValue = useRef(null)
+
+    //收集数据
+    const collectValue = ()=>{
+        console.log(inputValue.current.value);
+    }
+
     return (
         <div className={"NoteSection"}>
             <label>
                 <span>备注</span>
-                <input type="text" placeholder={"在这里添加备注"}/>
+                <input type="text" placeholder={"在这里添加备注"} ref={inputValue} onBlur={collectValue}/>
             </label>
         </div>
     )
