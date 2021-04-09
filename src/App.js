ae9584bae32dc292031d34b04b934b2bba97f263
xiao <779740337@qq.com>
2021-04-09 20:53:24 +0800

    将新加的account设置为当在支付页面的时候的消失

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react"
 import Nav from "./component/Nav"
 import Switch from "./component/SwitchContain"
 import "./App.scss"
+import {NavLink,useLocation} from "react-router-dom";
+import SvgIcon from "./component/SvgIcon";
+
 
 function App() {
-  return (
+    const location = useLocation().pathname
+    return (
     <div  className={"container"}>
         <main>
             <Switch/>
+            {location !== "/money" ? <NavLink to={"/money"} className={"account"}>
+                <SvgIcon name={`icon-account`} />
+            </NavLink> : ""}
         </main>
         <Nav/>
     </div>
